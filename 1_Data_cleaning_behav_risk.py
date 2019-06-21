b08ec3a810545325882977beceef7b0bf69ced2f
JaAy27 <jnax27@hotmail.com>
2019-06-21 16:37:38 +0100

    4:37pm commit

--- 1_Data_cleaning_behav_risk.py
+++ 1_Data_cleaning_behav_risk.py
@@ -38,22 +38,17 @@ for path in PATH_TO_APPEND_LIST:
         sys.path.append( path )
 
 # =============================================================================
-
 #IMPORTS - PYTHON MODULES
+        
 import pandas as pd
 import numpy as np
 from warnings import warn
 
 #User defined constants
-from constants import DATA, P
+from constants import DATA, TABLES, P
 
 # =============================================================================
 #IMPORTS 
-
-#TODO: Henrique -> After the comment line you can add a title like above and then
-#put a comment explaining the code. Names like "ec" and "br" may be quite cryptic
-#if you are reading this code a month from now. Names like entity_client and
-#behavioural_risk will take more space but be more understandable.
 
 with open(os.path.join(DATA, 'entities.csv')) as file:
     ent = pd.read_csv(file, sep=';')
@@ -67,27 +62,32 @@ with open(os.path.join(DATA, 'behavioural_risk.csv')) as file:
 with open(os.path.join(DATA, 'entity_client.csv')) as file:
     ent_client = pd.read_csv(file, sep=';')  
 
-#TODO: Henrique -> avoid leaving so many blank spaces, and try to be consistent
-# in the number of blank spaces you use. Also, why are you importing these tables?
-
-xls = pd.ExcelFile(os.path.join(DATA, 'Risk Tables.xlsx'))
-countrisk = pd.read_excel(xls, 'CountryRisk')
-compage = pd.read_excel(xls, 'CompanyAgeRisk')
-
-
 # =============================================================================
 ## DATA CLEANING
+## Verify unique accounts 
 
-## Verify unique accounts - clearing duplicates
+ent_client.client_number.value_counts().head()
+ent_client.entity_number.value_counts().head()
+ent_client['unique_entities'] = ent_client.client_number.astype(str).str.cat(
+    [ent_client.entity_number.astype(str)],sep='-')
+
 
 non_unique_entity_client = ent_client[ ent_client['unique_entities'].duplicated() ]
 if non_unique_entity_client.shape[0] > 0:
     warn('Non-unique combinations of entity and client have beeen detected')
+    
+acc.client_number.value_counts().head()
+acc.account_number.value_counts().head()
+acc['unique_accounts'] = acc.client_number.astype(str).str.cat(
+    [acc.account_number.astype(str)],sep='-')
+acc.unique_accounts.value_counts().head()
 
 non_unique_acc_client = acc[ acc['unique_accounts'].duplicated() ]
 if non_unique_acc_client.shape[0] > 0:
     warn('Non-unique combinations of entity and client have beeen detected')
 
+
+##I'm not sure what these do, but I'll leave it
 non_unique_behavioural_risk = behavioural_risk[ behavioural_risk['unique_behavioural_risk'].duplicated() ]
 if non_unique_behavioural_risk.shape[0] > 0:
     warn('Non-unique combinations of entity and client have beeen detected')
@@ -96,143 +96,74 @@ non_unique_ent = ent[ ent['unique_ent'].duplicated() ]
 if non_unique_ent.shape[0] > 0:
     warn('Non-unique combinations of entity and client have beeen detected')
 
-non_countrisk = countrisk[ countrisk['unique_countrisk'].duplicated() ]
-if non_unique_countrisk.shape[0] > 0:
-    warn('Non-unique combinations of entity and client have beeen detected')
 
+##Missing Values:
+n_records = len(ent)
+def missing_values_df(df):
+    for column in df:
+        print("{} | {} | {}".format(
+            column, len(df[df[column].isnull()]) / (1.0*n_records), df[column].dtype
+        ))
 
-#TODO: Henrique -> With pandas, it is better to do the following
-ent_client.drop('unique_entities', axis=1, inplace=True)
-acc.drop("unique_accounts", axis=1, inplace=True)
-behavioural_risk.drop("unique_behavioural_risk", axis=1, inplace=True)
-ent.drop("unique_ent", axis=1, inplace=True)
-countrisk.drop("unique_countrisk", axis=1, inplace=True)
+print(missing_values_df(ent))
 
+#Most of the missing values are related to entity type. So, private entities 
+#are missing risk that is related to companies while the latter are
+#missing risk related to private entities. 
 
-## Fill NA
-
-behavioural_risk.continuous_risk = behavioural_risk.continuous_risk.fillna(behavioural_risk.continuous_risk.mean())
-behavioural_risk.discrete_risk = behavioural_risk.discrete_risk.fillna(behavioural_risk.discrete_risk.mean())
-
-#If there are null values in the future, an improvement to the code needs to 
-#be made where we are filling the null values based off the mean values if it is
-#empresas or particulares. fill in with the means of those E and P values.
-
-#TODO: Not sure what this is for but kept in ~Lara
-ent_client[ent_client.duplicated()].shape
-countrisk[countrisk.duplicated()].shape
-
+#The dataset was divided into 2 files in the constants file: one for private
+# entities and another for enterprises. That will lower the amount of null values
+#in the data frame because not all of the columns are applicable to both 
+#"E" and "P" entities. They will then be saved as .csv files at the end of this code.
 # =============================================================================
 
 ##Data Quality Assurance
 
-#TODO: Henrique -> Again, what is the purpose of these lines without a print?
-print( ent.date_of_birth.value_counts().head() )
-#some of the values have a "0" birthdate.
+print( ent.date_of_birth.value_counts() )
+#Some of the values have a "0" birthdate.
 
-ent.sort_values(by = "date_of_birth").head(10)
-ent.loc[(ent['date_of_birth'] == 0)].shape
-ent.loc[(ent['date_of_birth'] == 0) & (ent["entity_type"] != "P")].shape
-ent.loc[(ent['date_of_birth'] == 0) & (ent["entity_type"] == "P")].shape
-ent.loc[(ent['date_of_birth'] < 0)]
+#How many entities have a birthdate of 0?
+print(ent.loc[(ent['date_of_birth'] == 0)].shape)
 
+#How many enterprise entities have a birthdate of 0? 
+#These make sense, companies do not have birthdates
+print(ent.loc[(ent['date_of_birth'] == 0) & (ent["entity_type"] != "P")].shape)
 
-#Here we split the data set into two dataframe by entity_type
-#This will lower the amount of null values in the data frame because not all of 
-#the columns are applicable to both "E" and "P" entities. They were then saved
-#as .csv files.
+#How many private entities have a birthdate of 0? 
+#These do not make sense -- people have birth dates and this is important 
+#information. These age risks should be flagged as 5 or removed from the data set
+print(ent.loc[(ent['date_of_birth'] == 0) & (ent["entity_type"] == "P")].shape)
 
+#We see that there are values with date of birth <0 as well
+print(ent.loc[(ent['date_of_birth'] < 0)])
 
-#TODO: Henrique -> This object is never used!
-#ent_by_type = ent.groupby("entity_type")
+#Remove the private entities with 0 as date of birth
+ent = ent[~((ent.entity_type == 'P') & ((ent.date_of_birth ==0)))]
 
-
-#TODO: Henrique -> Again, you are assuming someone is going to run this line
-#by line. You should either print, warn or raise an exception when you want
-#to ask the attention of the person running the code.
-
-ent_p = ent.loc[ent["entity_type"] == "P"]
-print( ent_p.isnull().sum() )
-ent_p.drop(["company_age_risk", "economic_activity_code_risk",
-            "society_type_risk"], axis = 1).head()
-
-ent_e = ent.loc[ent["entity_type"] == "E"]
-ent_e.isnull().sum()
-ent_e.drop(["nationality_risk", "occupation_risk", "qualifications_risk",
-            "age_risk"], axis = 1).head()
-
-#TODO: Henrique -> This is writing the csv in the wrong place. 
-#You should explicitly choose the path. Define a variable TABLES in your 
-#constants module that is TABLES = os.path.join(SCRIPTS, 'tables')
-#and write your csv there as I am doing in the code bellow
-#I also changed the name to something more understandable
-#You should also drop the columns you are not using in the model
-#you can do this by using the .drop(column_list, axis=1, inplace=True)
-with open( os.path.join(TABLES, 'private_entity_model.csv')) as file:
-    ent_p.to_csv(file, sep = ';')
-    
-with open( os.path.join(TABLES, 'enterprise_entity_model.csv')) as file:
-    ent_e.to_csv(file, sep = ';')
-
-
-#TODO: Henrique -> Do this before you export the datataset, so that the 
-#private and company model csv have the risk variable ready for the model
-#to use
+#Make sure all the date of births are positive:
+ent['date_of_birth'] = abs(ent['date_of_birth'])
 
 # =============================================================================
 ## ENTITY BEHAVIOURAL RISK
-# Determine entity risk
-
-
-    
-## Merge tables
-b_risk_acc = pd.merge(acc, behavioural_risk, on='account_number')
-
-
-## Associate client risk to maximum risk of accounts
-max_risk= b_risk_acc.groupby('client_number').max().continuous_risk
-risk_client = pd.merge(b_risk_acc, max_risk, on ='client_number')
-
-
-
-## Merge the resulting table with the entity_client table
-ent_b_risk = pd.merge(ent_client, risk_client, on='client_number')[['client_number',
-        'entity_number', 
-        'continuous_risk_y']].rename(columns={
-        "continuous_risk_y": 'risk_client'})
-
-
-## Count the number of entities that form each client
-count_entity = ent_b_risk.groupby('client_number').count().entity_number
-with_ent = pd.merge(ent_b_risk, count_entity, on='client_number').rename(columns={
-        "entity_number_y": 'count_ent'})
-
-
-    
-## Distribute the risk evenly among the entities
-with_ent['perc_risk'] = 1/with_ent['count_ent']
-with_ent['ent_b_risk'] = with_ent['risk_client'] * with_ent['perc_risk']
-
-
-
-## Remove and rename columns to get the final table
-final = with_ent[['entity_number_x', 'ent_b_risk']].rename(columns={
-        "entity_number_x": 'entity_number'})
-    
+#Determine entity risk
 
 client_size = ent_client.groupby('client_number').size().to_frame('client_size')
+
 #Add this to the dataframe (create a new one)
 df2 = ent_client.merge(client_size, left_on = 'client_number', right_index = True)
+
 #Compute inverses
 df2['weight_aux'] = 1/df2['client_size']
+
 #Now add all auxiliary weights to normalize
 weight_aux_sum = df2.groupby('client_number')['weight_aux'].sum(
         ).to_frame('weight_aux_sum')
+
 #Add the column to the dataframe and compute final weight
 df2 = df2.merge(weight_aux_sum, left_on = 'client_number', right_index = True)
 df2['weight'] = df2['weight_aux']/df2['weight_aux_sum']
 
-
+#Determine a function to calculate the weighted holder mean:
 def weighted_holder(series, p=P, weight = None):
     """
     Function to compute the weighted Holder average
@@ -272,62 +203,80 @@ def weighted_holder(series, p=P, weight = None):
     
     return result
 
-#TODO: Assign risk to the entities using the weighted holder mean
+#Assign risk to the entities using the weighted holder mean
 ent_client = ent_client.drop(['is_first'], axis=1)
 behavioural_risk = behavioural_risk.drop(['discrete_risk'], axis=1)
-
 #br_df = br_df.drop(columns = ['continuous_risk', 'cluster' ], axis=1)
-
-
-
 ec_acc_df = pd.merge(ent_client,acc, left_on="client_number", 
                      right_on="client_number")
 
-
-
-#creating one large dataset connecting entity number to behavioral risk
+#Creating one large dataset connecting entity number to behavioral risk
 entity_behav_risk_df = pd.merge(behavioural_risk, ec_acc_df, left_on="account_number", 
                                 right_on="account_number", how='inner')
 
-#reordering the columns
+#Reordering the columns
 entity_behav_risk_df = entity_behav_risk_df[['entity_number','client_number',
                             'account_number','cluster','continuous_risk']]
+    
+#entity_behav_risk_df.to_csv("entity_behavioral_risk.csv")
+holder = entity_behav_risk_df.groupby('entity_number')['continuous_risk'].agg(
+        weighted_holder)
 
-#dormant_acc = entity_behav_risk_df[entity_behav_risk_df.cluster == 660]
+final = pd.merge(entity_behav_risk_df, holder, on='entity_number')
+final = final[['entity_number', 'continuous_risk_y', 'client_number', 
+               'account_number']].rename(columns={
+               "continuous_risk_y":'continuous_risk'})
+print(final)
 
 
+##Divide the file into 2: one for the private entities and the other for
+#the companies:
+
+final2 = pd.merge(final, ent, on='entity_number')
+final2 = final2[['entity_number', 'client_number','account_number',
+                 'continuous_risk','entity_type']]
 
 
-#TODO: accomodate for future immature values by determining if the account 
+#Accommodate for future immature values by determining if the account 
 #is tied to a private or enterprise, then assigning an average value for 
-#the risk based of if it is an enterprise or if it is a private 
+#the risk based on if it is an enterprise or if it is private 
+ent_p = final2[final2.entity_type == 'P']
+ent_e = final2[final2.entity_type == 'E']
 
-for seg, behav_aux in entity_behav_risk_df.groupby('continuous_risk'):
+for seg, behav_aux in ent_p.groupby('continuous_risk'):
         null_count = ( behav_aux.isnull().sum())
 
 n_missing = null_count['continuous_risk']
 if n_missing > 0:
-    warn ("There are missing values")
+    warn("There are missing values in the table for private entities.")    
+ent_p.continuous_risk = ent_p.continuous_risk.fillna(ent_p.continuous_risk.mean())
 
 
+for seg, behav_aux in ent_e.groupby('continuous_risk'):
+        null_count1 = ( behav_aux.isnull().sum())
 
-entity_behav_risk_df.to_csv("entity_behavioral_risk.csv")
-    
-holder_risk = entity_behav_risk_df.groupby('entity_number', 
-                    as_index=False).apply(weighted_holder)
-#OR: weighted_risk = holder_risk.groupby('entity_number')['continuous_risk'].agg(weighted_holder)
-final = pd.merge(entity_behav_risk_df, holder_risk, on='entity_number')
-final= final[['entity_number', 'continuous_risk_y']].rename(columns={
-        "continuous_risk_y": 'continuous_risk'})
-print(final)
+n_missing = null_count['continuous_risk']
+if n_missing > 0:
+    warn("There are missing values in the table for private entities.")        
+ent_e.continuous_risk = ent_e.continuous_risk.fillna(ent_e.continuous_risk.mean())
 
-#TODO: Check if it is correct (manually for 1 or 2 cases - it will also help
+#Export them into 2 csv files
+with open( os.path.join(TABLES, 'private_entity_model.csv'), 'w') as file:
+    ent_p.to_csv(file, sep = ';')
+
+with open( os.path.join(TABLES, 'enterprise_entity_model.csv'), 'w') as file:
+    ent_e.to_csv(file, sep = ';')
+
+#Check if it is correct (manually for 1 or 2 cases - it will also help
 #you to get a feel of the impact of the choice of p)
+#49994 entity
+ent_49994 = np.sqrt((0.119944**2)/2) #correct
 
+#50000
+ent_50000 = np.sqrt((0.081082**2)/2) #correct
 
-
-
-
+#9:
+ent_9 = np.sqrt((0.081082**2)/2) #correct
 
 
 
